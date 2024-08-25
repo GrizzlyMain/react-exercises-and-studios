346c1e453b198c706a7a0ca8229975e7082f6004
GrizzlyMain <ryan.thom.dalton@gmail.com>
2024-08-25 11:25:48 -0500

    excercise

--- part1/exercises/src/components/BookList.jsx
+++ part1/exercises/src/components/BookList.jsx
@@ -1,15 +1,15 @@
 export default function BookList() {
-   let pageTitle = "shrug";
-   let book1 = "google.com";
-   let book2 = "";
-   let book3 = "";
+   let pageTitle = "Bro Books";
+   let book1 = "https://upload.wikimedia.org/wikipedia/en/d/df/Blood_Meridian_Cormac_McCarthy_book_cover.png";
+   let book2 = "https://m.media-amazon.com/images/I/91MBNyp411L._AC_UF1000,1000_QL80_.jpg";
+   let book3 = "https://cdn11.bigcommerce.com/s-q39b4/images/stencil/2000x2000/products/7431/223400/9780804830805.website__94202.1567110692.jpg?c=2";
 
    return (
       <div>
          <h3>{pageTitle}</h3>
-         <img src={book1} alt="Nothing here!" />
-         <img src={book2} alt="Nothing here!" />
-         <img src={book3} alt="Nothing here!" />
+         <img src={book1} alt="Blood Meridian" width="100" height="100" />
+         <img src={book2} alt="Fight Club" width="100" height="100"/>
+         <img src={book3} alt="The Art of War" width="100" height="100" />
       </div>      
    );
 }
--- part1/exercises/src/components/ChoresList.jsx
+++ part1/exercises/src/components/ChoresList.jsx
@@ -1,3 +1,24 @@
-export default function ChoresList () {
-   return <p>No content is here!</p>;
-}
+import React from 'react';
+import classes from './ChoresList.module.css';
+
+export default function ChoresList() {
+      const chores = ["Mow Lawn", "Stain Fence", "Plant Seeds", "Mulch Beds"];
+      return (
+   
+      <div>
+         <h3 className = {classes.choresHeading}>Chores</h3>
+         <ol>
+         <li className={classes.choresText} for={chores[0]}>
+            <input type="checkbox" id={chores[0]} />
+           {chores[0]}</li>
+           <li className={classes.choresText} for={chores[1]}><input type="checkbox" id={chores[1]} />
+            {chores[1]}</li>
+            <li className={classes.choresText} for={chores[2]}><input type="checkbox" id={chores[2]} />
+            {chores[2]}</li>
+            <li className={classes.choresText} for={chores[3]}> <input type="checkbox" id={chores[3]} />
+            {chores[3]}</li>
+         </ol>
+      </div>
+      
+      );
+   }
